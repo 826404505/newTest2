4af98c514912dce8cc0a15523e6ab83bbfdc282e
826404505 <826404505@qq.com>
2019-04-16 11:36:15 +0800

    first sql

---
+++ src/main/java/com/zsq/testblock/TestblockApplication.java
@@ -0,0 +1,13 @@
+package com.zsq.testblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestblockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestblockApplication.class, args);
+    }
+
+}
